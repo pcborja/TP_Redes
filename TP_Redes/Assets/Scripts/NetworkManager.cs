b4a2e019580b902ed7fadaf41efed0a8eb8740dd
Pablo Borja <pborja@jamcity.com>
2019-10-04 16:46:45 -0300

    Update scenes

---
+++ TP_Redes/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,40 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ConnectToServerButton()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+    
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRoom("Lobby");
+    }
+    
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("GameLevel");
+    }
+    
+    public override void OnCreatedRoom()
+    {
+        
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
